cdd72d3900ed97b8ab7117cda2b448a9ca56fb55
MOOW4 <108760540+MOOW4@users.noreply.github.com>
2023-07-02 15:05:21 +0200

    add post

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ const bodyParser = require("body-parser");
 const request = require("request");
 
 const app = express();
+app.use(express.static("public"));// other files used in .html wouldn't load without this, all paths start inside "public" folder
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.get("/", (req,res) => {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req,res) => {
+    /* console.log(req.body.firstName);
+    console.log(req.body.lastName);
+    console.log(req.body.email); */
+    
+}); 
 
 app.listen(3000, () =>{
     console.log("Server is up and running on port 3000");
